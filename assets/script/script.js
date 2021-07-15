d9c145836eecac6a37792112cdab3908bac6e9ba
Renato Novaes <renatoalmeida49@gmail.com>
2021-07-15 09:56:26 -0300

    First version of calculator implemented

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,6 +1,9 @@
 let billInput = document.querySelector('#bill')
 let peopleInput = document.querySelector('#people-number')
 
+let tipPerPersonValue = document.querySelector('#tipPerPersonValue')
+let billPerPersonValue = document.querySelector('#billPerPersonValue')
+
 billInput.addEventListener('keyup', () => {
     calcTip()
 })
@@ -16,11 +19,26 @@ function calcTip() {
 
     if (parseInt(peopleInput.value) === 0 || Number.isNaN(parseInt(peopleInput.value))) {
         console.log('Não existe divisão por zero. Você quer criar um buraco negro por acaso?')
+        peopleInput.classList.add('error')
         return
     }
 
+    peopleInput.classList.remove('error')
+
+    let tipPerPerson = (totalBill - billInput.value) / parseInt(peopleInput.value)
     let valuePerPerson = totalBill / parseInt(peopleInput.value)
-    
-    console.log("Total bill: ", totalBill)
-    console.log("Valor por pessoa: ", valuePerPerson.toFixed(2))
+
+    formater(tipPerPerson, valuePerPerson)
+}
+
+function formater(tipPerPerson, valuePerPerson) {
+    tipPerPersonValue.innerHTML = tipPerPerson.toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD",
+      })
+
+    billPerPersonValue.innerHTML = valuePerPerson.toLocaleString("en-US", {
+        style: "currency",
+        currency: "USD",
+      })
 }
